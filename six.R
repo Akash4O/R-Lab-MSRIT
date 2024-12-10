34fc7bb5be9e5bf218078613529cc8f1302fa8a9
Akash4O <akashkolde1320@gmail.com>
2024-12-11 01:21:57 +0530

    Sixth program Added

---
+++ six.R
@@ -0,0 +1,17 @@
+library(forecast)
+
+sales_data <- data.frame(
+  Month = seq(as.Date("2023-01-01"), as.Date("2023-06-01"),by="months"),
+  Sales = c(12000,15000,18000,16000,20000,22000)
+)
+
+sales_ts <- ts(sales_data$Sales,frequency = 12,start = c(2023,1))
+print(sales_ts)
+
+arima_model <- auto.arima(sales_ts)
+
+forecast_result <- forecast(arima_model,h=3)
+
+print(forecast_result)
+
+plot(forecast_result)
